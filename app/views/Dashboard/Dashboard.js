646947bb35c480e03489b65bd69b5bf55abaade6
AshotN <hego556@yahoo.com>
2017-08-18 14:06:37 -0700

    forgot this

---
+++ app/views/Dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+/**
+ * Created by hego555 on 6/15/17.
+ */
+import React, {Component} from 'react';
+
+export default class DashboardView extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<div>
+				<h3>Hello World!</h3>
+			</div>
+		);
+	}
+}
+
